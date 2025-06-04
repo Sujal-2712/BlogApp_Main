19263109c32952355d0213cf6bd95d887b82db2d
Sujal-Satva <sujalkareliya27@gmail.com>
2025-06-04 23:20:03 +0530

    fix url

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 
-const API_URL = "https://blogapp-main.onrender.com/";
+const API_URL = "https://blogapp-main.onrender.com";
 
 export { API_URL };
 // https://blogapp01-heaub0apdxa6b8db.centralindia-01.azurewebsites.net
